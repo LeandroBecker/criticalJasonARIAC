f45cac5b511206753ad40007583f096f86d494f7
LeandroBecker <leandro.becker@ufsc.br>
2023-03-30 10:28:49 +0100

    Initial commit, example working properly

---
+++ exec110s.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo "Running Critical Jason in : "
+pwd 
+
+SECONDS_TO_RUN=110  # Number of seconds to run
+
+END_TIME=$((SECONDS_TO_RUN + $(date +%s)))
+
+for i in {0..0};
+do
+	./gradlew runHelp -q --console=plain  > outp.tmp &
+	sleep 3
+	PID_JAS=$(jps | grep RunLocalMAS | awk 'NR==1{print $1}')
+	ros2 topic pub /ariac/start_human std_msgs/msg/Bool '{data: true}' --once 
+	while [ $(date +%s) -lt $END_TIME ]; do 
+	    # Do something while Jason is running (the file doesn't exist)
+	    ps -p $PID_JAS -o %cpu 
+	    sleep 1
+	done
+	ros2 topic pub /ariac_human/go_home std_msgs/msg/Bool '{data: true}' --once 
+	touch .stop___MAS
+done
+python3 test.py 0
+
---
+++ readmeGgradle.txt
@@ -0,0 +1,6 @@
+The file that gradlew needs to run is called 'build.gradle'
+
+I have two possibilities, that are renamed to the needed one according to the experiment:
+1) build-gradle.runLocal: created to support running the local Jason (CBS Jason)
+2) build-gradle.standard: standard one, calls Jason on its repository
+
