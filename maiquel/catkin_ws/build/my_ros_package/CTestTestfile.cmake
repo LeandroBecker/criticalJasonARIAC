a57425f2460e49ffa6671f7dae985b48d41b9798
LeandroBecker <leandro.becker@ufsc.br>
2023-04-06 16:46:42 +0100

    Deleted a couple of files

--- maiquel/catkin_ws/build/my_ros_package/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/leandro/ariac_ws/src/ARIAC/ariac_human/criticalJasonARIAC/maiquel/catkin_ws/src/my_ros_package
-# Build directory: /home/leandro/ariac_ws/src/ARIAC/ariac_human/criticalJasonARIAC/maiquel/catkin_ws/build/my_ros_package
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
